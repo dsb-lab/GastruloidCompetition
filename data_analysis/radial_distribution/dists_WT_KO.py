4ae239602fa3034d8aacff600a1a6f8afb35d106
Pablo Casani Galdon <pablocasani@gmail.com>
2025-01-09 11:06:41 +0100

    preparing data for manuscript, local

--- data_analysis/radial_distribution/dists_WT_KO.py
+++ data_analysis/radial_distribution/dists_WT_KO.py
@@ -9,9 +9,9 @@ from scipy.ndimage import gaussian_filter1d
 
 model = StarDist2D.from_pretrained('2D_versatile_fluo')
 
-# path_figures = "/home/pablo/Desktop/PhD/projects/GastruloidCompetition/thesis/figures/spatial_distribution/"
+path_figures = "/home/pablo/Desktop/PhD/projects/GastruloidCompetition/thesis/figures/spatial_distribution/"
 
-path_figures = "/home/pablo/Desktop/PhD/projects/GastruloidCompetition/figures/figures_joshi_poster/"
+# path_figures = "/home/pablo/Desktop/PhD/projects/GastruloidCompetition/figures/figures_joshi_poster/"
 
 DISTS_F3_WT = []
 DISTS_A12_WT = []
@@ -22,6 +22,10 @@ DISTS_A12_KO = []
 DISTS_apo_KO = []
 
 TIMES = ["48hr", "72hr", "96hr"]
+
+all_files = []
+all_data = []
+
 for TIME in TIMES:
     dists_F3 = []
     dists_A12 = []
@@ -53,9 +57,13 @@ for TIME in TIMES:
         dists_centroid_F3_current = np.load(file_path+"_dists_centroid_F3.npy")
         
         dists = dists_centroid_F3_current / (dists_centroid_F3_current + dists_contour_F3_current)
+        all_files.append(file + " F3")
+        all_data.append(dists)
         dists_F3 = [*dists_F3, *dists]
         
         dists = dists_centroid_A12_current / (dists_centroid_A12_current + dists_contour_A12_current)
+        all_files.append(file + " A12")
+        all_data.append(dists)
         dists_A12 = [*dists_A12, *dists]
         
         dists = dists_centroid_Casp3_current / (dists_centroid_Casp3_current + dists_contour_Casp3_current)
@@ -98,9 +106,13 @@ for TIME in TIMES:
         dists_centroid_F3_current = np.load(file_path+"_dists_centroid_F3.npy")
         
         dists = dists_centroid_F3_current / (dists_centroid_F3_current + dists_contour_F3_current)
+        all_files.append(file + " F3")
+        all_data.append(dists)
         dists_F3 = [*dists_F3, *dists]
         
         dists = dists_centroid_A12_current / (dists_centroid_A12_current + dists_contour_A12_current)
+        all_files.append(file + " A12")
+        all_data.append(dists)
         dists_A12 = [*dists_A12, *dists]
         
         dists = dists_centroid_Casp3_current / (dists_centroid_Casp3_current + dists_contour_Casp3_current)
@@ -110,6 +122,23 @@ for TIME in TIMES:
     DISTS_F3_WT.append(dists_F3)
     DISTS_A12_WT.append(dists_A12)
     DISTS_apo_WT.append(dists_Casp3)
+
+
+import csv
+output_file = path_figures+"data_dists_WT_KO.csv"
+
+full_data = []
+for f in range(len(all_files)):
+    dat = [all_files[f], *all_data[f]]
+    full_data.append(dat)
+
+# Output CSV file path
+
+# Write to CSV
+with open(output_file, mode="w", newline="") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    # Write the header
+    csv_writer.writerows(full_data)
 
 
 import matplotlib as mpl
@@ -125,7 +154,7 @@ mpl.rc('legend', fontsize=18)
 
 import matplotlib.pyplot as plt
 
-density=False
+density=True
 fig, ax = plt.subplots(2,3, figsize=(12,6),sharex=True)
 for t, TIME in enumerate(TIMES):
     
@@ -155,6 +184,7 @@ plt.tight_layout()
 # plt.savefig(path_figures+"dists_condition.svg")
 # plt.savefig(path_figures+"dists_condition.pdf")
 plt.show()
+
 
 
 bin_ns = range(20, 60, 5)
@@ -279,10 +309,80 @@ for density in [True, False]:
 # plt.show()
 
 
+TIMES = ["48hr", "72hr", "96hr"]
+
+all_files = []
+all_data = []
+
+for TIME in TIMES:
+    for stage in ["early", "mid", "late"]:
+        dists_Casp3 = []
+        
+        ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
+        path_data_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/stacks/{}/KO/'.format(TIME)
+        path_save_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/ctobjects/{}/KO/'.format(TIME)
+        path_save_results='/home/pablo/Desktop/PhD/projects/GastruloidCompetition/results/radial_distribution/{}_apoptosis/{}/KO/'.format(stage, TIME)
+
+        ### GET FULL FILE NAME AND FILE CODE ###
+        files = get_file_names(path_data_dir)
+
+        channel_names = ["F3", "A12", "DAPI", "Casp3", "BF"]
+        if "96hr" in path_data_dir:
+            channel_names = ["A12", "F3", "Casp3", "BF", "DAPI"]
+
+        for f, file in enumerate(files):
+            path_data = path_data_dir+file
+            file, embcode = get_file_name(path_data_dir, file, allow_file_fragment=False, return_files=False, return_name=True)
+
+            file_path = path_save_results+embcode
+            dists_contour_Casp3_current = np.load(file_path+"_dists_contour_Casp3.npy")
+            dists_centroid_Casp3_current = np.load(file_path+"_dists_centroid_Casp3.npy")
+            
+            dists = dists_centroid_Casp3_current / (dists_centroid_Casp3_current + dists_contour_Casp3_current)
+            all_files.append(file + " - Casp3 - " + stage)
+            all_data.append(dists)
+
+        ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
+        path_data_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/stacks/{}/WT/'.format(TIME)
+        path_save_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/ctobjects/{}/WT/'.format(TIME)
+        path_save_results='/home/pablo/Desktop/PhD/projects/GastruloidCompetition/results/radial_distribution/{}_apoptosis/{}/WT/'.format(stage, TIME)
+
+        ### GET FULL FILE NAME AND FILE CODE ###
+        files = get_file_names(path_data_dir)
+
+        for f, file in enumerate(files):
+            path_data = path_data_dir+file
+            file, embcode = get_file_name(path_data_dir, file, allow_file_fragment=False, return_files=False, return_name=True)
+
+            file_path = path_save_results+embcode
+            dists_contour_Casp3_current = np.load(file_path+"_dists_contour_Casp3.npy")
+            dists_centroid_Casp3_current = np.load(file_path+"_dists_centroid_Casp3.npy")
+
+            dists = dists_centroid_Casp3_current / (dists_centroid_Casp3_current + dists_contour_Casp3_current)
+            all_files.append(file + "- Casp3 - " + stage)
+            all_data.append(dists)        
+
+
+import csv
+output_file = path_figures+"data_dists_WT_KO_apo.csv"
+
+full_data = []
+for f in range(len(all_files)):
+    dat = [all_files[f], *all_data[f]]
+    full_data.append(dat)
+
+# Output CSV file path
+
+# Write to CSV
+with open(output_file, mode="w", newline="") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    # Write the header
+    csv_writer.writerows(full_data)
+    
 # import matplotlib.pyplot as plt
 # fig, ax = plt.subplots(1,2, figsize=(10,3.5))
 # for t, TIME in enumerate(TIMES):
-    
+
 #     dists = [*DISTS_F3_WT[t], *DISTS_A12_WT[t], *DISTS_F3_KO[t], *DISTS_A12_KO[t]]
 #     ax[0].hist(dists, alpha=0.5, bins=50, density=density, label=TIME)
 #     ax[0].set_yticks([])
