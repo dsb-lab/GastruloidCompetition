2a0417cc7f560d66a078952e21f52575032e1e37
PabloCasaniGaldon <pablocasani@gmail.com>
2025-08-29 10:19:24 +0200

    Added file structure for p53 analysis and segmentation is done

---
+++ data_analysis/p53/debris_threshold.py
@@ -0,0 +1,177 @@
+### LOAD PACKAGE ###
+from qlivecell import get_file_name, cellSegTrack, check_or_create_dir, get_file_names
+import numpy as np
+import matplotlib.pyplot as plt
+
+### LOAD STARDIST MODEL ###
+from stardist.models import StarDist2D
+model = StarDist2D.from_pretrained('2D_versatile_fluo')
+
+# ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
+CONDS = ["WT", "KO"]
+repeats = ["n2", "n3", "n4"]
+
+areas = []
+
+for COND in CONDS:
+    for REP in repeats:
+        
+        path_data_dir="/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/p53_analysis/input/{}/{}/".format(COND,REP)
+        path_save_dir="/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/p53_analysis/segobjects/{}/{}/".format(COND,REP)
+
+        check_or_create_dir(path_save_dir)
+        
+        files = get_file_names(path_data_dir)
+        
+        channel_names = ["A12", "p53", "F3", "DAPI"]
+
+        for f, file in enumerate(files):
+            
+            path_data = path_data_dir+file
+            file, embcode = get_file_name(path_data_dir, file, allow_file_fragment=False, return_files=False, return_name=True)
+            path_save = path_save_dir+embcode
+                
+            check_or_create_dir(path_save)
+
+            ### DEFINE ARGUMENTS ###
+            segmentation_args={
+                'method': 'stardist2D', 
+                'model': model, 
+                'blur': [2,1], 
+                'min_outline_length':100,
+            }
+
+            concatenation3D_args = {
+                'do_3Dconcatenation': False
+            }
+
+            error_correction_args = {
+                'backup_steps': 10,
+                'line_builder_mode': 'points',
+            }
+            
+            ch = channel_names.index("F3")
+            
+            batch_args = {
+                'name_format':"ch"+str(ch)+"_{}",
+                'extension':".tif",
+            } 
+            plot_args = {
+                'plot_layout': (1,1),
+                'plot_overlap': 1,
+                'masks_cmap': 'tab10',
+                'plot_stack_dims': (256, 256), 
+                'plot_centers':[False, False], # [Plot center as a dot, plot label on 3D center]
+                'channels':[ch],
+                'min_outline_length':75,
+            }
+            
+            chans = [ch]
+            for _ch in range(len(channel_names)):
+                if _ch not in chans:
+                    chans.append(_ch)
+                    
+            CT_F3 = cellSegTrack(
+                path_data,
+                path_save,
+                segmentation_args=segmentation_args,
+                concatenation3D_args=concatenation3D_args,
+                error_correction_args=error_correction_args,
+                plot_args=plot_args,
+                batch_args=batch_args,
+                channels=chans
+            )
+
+            CT_F3.load()
+            
+            for cell in CT_F3.jitcells:
+                zc = int(cell.centers[0][0])
+                zcid = cell.zs[0].index(zc)
+
+                mask = cell.masks[0][zcid]
+                area = len(mask)* CT_F3.CT_info.xyresolution**2
+                areas.append(area)
+
+                        
+            ch = channel_names.index("A12")
+            batch_args = {
+                'name_format':"ch"+str(ch)+"_{}",
+                'extension':".tif",
+            }
+            
+            chans = [ch]
+            for _ch in range(len(channel_names)):
+                if _ch not in chans:
+                    chans.append(_ch)
+
+            CT_A12 = cellSegTrack(
+                path_data,
+                path_save,
+                segmentation_args=segmentation_args,
+                concatenation3D_args=concatenation3D_args,
+                error_correction_args=error_correction_args,
+                plot_args=plot_args,
+                batch_args=batch_args,
+                channels=chans
+            )
+
+            CT_A12.load()
+
+            for cell in CT_A12.jitcells:
+                zc = int(cell.centers[0][0])
+                zcid = cell.zs[0].index(zc)
+
+                mask = cell.masks[0][zcid]
+                area = len(mask)* CT_A12.CT_info.xyresolution**2
+                areas.append(area)
+                
+
+
+import matplotlib as mpl
+plt.rcParams.update({
+    "text.usetex": True,
+})
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{siunitx} \sisetup{detect-all} \usepackage{helvet} \usepackage{sansmath} \sansmath'
+mpl.rc('font', size=14) 
+mpl.rc('axes', labelsize=14) 
+mpl.rc('xtick', labelsize=14) 
+mpl.rc('ytick', labelsize=14) 
+mpl.rc('legend', fontsize=14) 
+
+from scipy.signal import argrelextrema
+from sklearn.neighbors import KernelDensity
+
+
+fig, ax = plt.subplots(2,1, figsize=(5,8))
+threshold = 0
+data = np.array(areas)
+ax[0].hist(data, bins=200, color=[0.0, 0.8, 0.0], density=True, alpha=0.6,)
+
+x = np.arange(0, step=0.1, stop=np.max(data))
+bw = 5
+modelo_kde = KernelDensity(kernel="linear", bandwidth=bw)
+modelo_kde.fit(X=data.reshape(-1, 1))
+densidad_pred = np.exp(modelo_kde.score_samples(x.reshape((-1, 1))))
+ax[0].plot(x, densidad_pred, color="magenta")
+
+local_minima = argrelextrema(densidad_pred, np.less)[0]
+threshold = x[local_minima[0]]
+x_th = np.ones(len(x)) * x[local_minima[0]]
+y_th = np.linspace(0, np.max(densidad_pred), num=len(x))
+ax[0].plot(x_th, y_th, c="k", ls="--",lw=2, label="debris th.")
+
+ax[0].set_ylabel("count")
+ax[1].set_ylabel("count")
+
+ax[1].hist(data, bins=200, color=[0.0, 0.8, 0.0], density=True, alpha=0.6)
+ax[1].set_xlabel(r"area ($\mu$m$^2$)")
+ax[1].plot(x, densidad_pred, color="magenta")
+ax[1].plot(x_th, y_th, c="k", ls="--",lw=2, label="debris th.")
+
+ax[1].set_xlim(-1, 75)
+plt.tight_layout()
+
+path_figs = "/home/pablo/Desktop/PhD/projects/GastruloidCompetition/results/p53/"
+plt.savefig(path_figs+"debris_thresholds.svg")
+plt.savefig(path_figs+"debris_thresholds.pdf")
+plt.show()
---
+++ data_analysis/radial_distribution/radial_dists_metrics.py/KD_test_radial_dists.py
@@ -0,0 +1,254 @@
+### LOAD PACKAGE ###
+from qlivecell import get_file_name, cellSegTrack, get_file_names, correct_path
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+### LOAD STARDIST MODEL ###
+from stardist.models import StarDist2D
+from scipy.ndimage import gaussian_filter1d
+
+import matplotlib as mpl
+plt.rcParams.update({
+    "text.usetex": True,
+})
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{siunitx} \sisetup{detect-all} \usepackage{helvet} \usepackage{sansmath} \sansmath'
+mpl.rc('font', size=18) 
+mpl.rc('axes', labelsize=18) 
+mpl.rc('xtick', labelsize=18) 
+mpl.rc('ytick', labelsize=18) 
+mpl.rc('legend', fontsize=18) 
+
+import matplotlib.pyplot as plt
+
+model = StarDist2D.from_pretrained('2D_versatile_fluo')
+
+path_figures = "/home/pablo/Desktop/PhD/projects/GastruloidCompetition/results/radial_distribution/center_to_edge_binned/"
+
+DISTS_F3_WT = []
+DISTS_A12_WT = []
+DISTS_apo_WT = []
+
+DISTS_F3_KO = []
+DISTS_A12_KO = []
+DISTS_apo_KO = []
+
+TIMES = ["48hr", "72hr", "96hr"]
+
+all_files = []
+all_data = []
+
+for TIME in TIMES:
+    dists_F3 = []
+    dists_A12 = []
+    dists_Casp3 = []
+    
+    ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
+    path_data_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/stacks/{}/KO/'.format(TIME)
+    path_save_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/ctobjects/{}/KO/'.format(TIME)
+    path_save_results='/home/pablo/Desktop/PhD/projects/GastruloidCompetition/results/radial_distribution/early_apoptosis/{}/KO/'.format(TIME)
+
+    ### GET FULL FILE NAME AND FILE CODE ###
+    files = get_file_names(path_data_dir)
+
+    channel_names = ["F3", "A12", "DAPI", "Casp3", "BF"]
+    if "96hr" in path_data_dir:
+        channel_names = ["A12", "F3", "Casp3", "BF", "DAPI"]
+
+    for f, file in enumerate(files):
+        path_data = path_data_dir+file
+        file, embcode = get_file_name(path_data_dir, file, allow_file_fragment=False, return_files=False, return_name=True)
+
+        file_path = path_save_results+embcode
+        dists_contour_Casp3_current = np.load(file_path+"_dists_contour_Casp3.npy")
+        dists_contour_A12_current = np.load(file_path+"_dists_contour_A12.npy")
+        dists_contour_F3_current = np.load(file_path+"_dists_contour_F3.npy")
+        
+        dists_centroid_Casp3_current = np.load(file_path+"_dists_centroid_Casp3.npy")
+        dists_centroid_A12_current = np.load(file_path+"_dists_centroid_A12.npy")
+        dists_centroid_F3_current = np.load(file_path+"_dists_centroid_F3.npy")
+        
+        dists = dists_centroid_F3_current / (dists_centroid_F3_current + dists_contour_F3_current)
+        all_files.append(file + " F3")
+        all_data.append(dists)
+        dists_F3.append(dists)
+        
+        dists = dists_centroid_A12_current / (dists_centroid_A12_current + dists_contour_A12_current)
+        all_files.append(file + " A12")
+        all_data.append(dists)
+        dists_A12.append(dists)
+
+        dists = dists_centroid_Casp3_current / (dists_centroid_Casp3_current + dists_contour_Casp3_current)
+        dists_Casp3.append(dists)
+                
+    DISTS_F3_KO.append(dists_F3)
+    DISTS_A12_KO.append(dists_A12)
+    DISTS_apo_KO.append(dists_Casp3)
+
+    ### PATH TO YOU DATA FOLDER AND TO YOUR SAVING FOLDER ###
+    path_data_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/stacks/{}/WT/'.format(TIME)
+    path_save_dir='/home/pablo/Desktop/PhD/projects/Data/gastruloids/joshi/competition/2023_11_17_Casp3/ctobjects/{}/WT/'.format(TIME)
+    path_save_results='/home/pablo/Desktop/PhD/projects/GastruloidCompetition/results/radial_distribution/early_apoptosis/{}/WT/'.format(TIME)
+
+
+    ### GET FULL FILE NAME AND FILE CODE ###
+    files = get_file_names(path_data_dir)
+
+    dists_F3 = []
+    dists_A12 = []
+    dists_Casp3 = []
+    
+    channel_names = ["F3", "A12", "DAPI", "Casp3", "BF"]
+    if "96hr" in path_data_dir:
+        channel_names = ["A12", "F3", "Casp3", "BF", "DAPI"]
+
+    for f, file in enumerate(files):
+        path_data = path_data_dir+file
+        file, embcode = get_file_name(path_data_dir, file, allow_file_fragment=False, return_files=False, return_name=True)
+
+        file_path = path_save_results+embcode
+        
+        dists_contour_Casp3_current = np.load(file_path+"_dists_contour_Casp3.npy")
+        dists_contour_A12_current = np.load(file_path+"_dists_contour_A12.npy")
+        dists_contour_F3_current = np.load(file_path+"_dists_contour_F3.npy")
+        
+        dists_centroid_Casp3_current = np.load(file_path+"_dists_centroid_Casp3.npy")
+        dists_centroid_A12_current = np.load(file_path+"_dists_centroid_A12.npy")
+        dists_centroid_F3_current = np.load(file_path+"_dists_centroid_F3.npy")
+        
+        dists = dists_centroid_F3_current / (dists_centroid_F3_current + dists_contour_F3_current)
+        all_files.append(file + " F3")
+        all_data.append(dists)
+        dists_F3.append(dists)
+        
+        dists = dists_centroid_A12_current / (dists_centroid_A12_current + dists_contour_A12_current)
+        all_files.append(file + " A12")
+        all_data.append(dists)
+        dists_A12.append(dists)
+
+        dists = dists_centroid_Casp3_current / (dists_centroid_Casp3_current + dists_contour_Casp3_current)
+        dists_Casp3.append(dists)
+    
+
+    DISTS_F3_WT.append(dists_F3)
+    DISTS_A12_WT.append(dists_A12)
+    DISTS_apo_WT.append(dists_Casp3)
+
+from scipy.stats import ks_2samp, combine_pvalues
+
+p_values = []
+ks_results = []
+for i in range(len(DISTS_F3_WT[0])):
+    A = DISTS_F3_WT[0][i]
+    B = DISTS_A12_WT[0][i]
+    ks_stat, p_val = ks_2samp(A, B)
+    p_values.append(p_val)
+    ks_results.append((i, ks_stat, p_val))
+    print(f"Experiment {i}: KS={ks_stat:.3f}, p={p_val:.3e}")
+
+# Combine p-values using Fisher's method
+chi2_stat, combined_p = combine_pvalues(p_values, method='fisher')
+
+
+print("\n--- Meta-analysis result ---")
+print(f"Fisher's method chi2={chi2_stat:.3f}, combined p={combined_p:.3e}")
+
+bins = 50
+density=True
+
+fig,ax = plt.subplots()
+ax.hist(A, bins=bins, density=density, alpha=0.5)
+ax.hist(B, bins=bins, density=density, alpha=0.5)
+
+plt.show()
+
+A = DISTS_F3_WT[0][0]
+B = DISTS_A12_WT[0][1]
+ks_stat, p_val = ks_2samp(A, B)
+p_values.append(p_val)
+ks_results.append((i, ks_stat, p_val))
+print(f"Experiment {i}: KS={ks_stat:.3f}, p={p_val:.3e}")
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# -------------------------
+# Example data (replace with your real data)
+# -------------------------
+# np.random.seed(42)
+# experiments = [
+#     (np.random.normal(0, 1, 60), np.random.normal(0.1, 1, 55)),  # Experiment 1
+#     (np.random.normal(0, 1, 70), np.random.normal(0.05, 1, 80)), # Experiment 2
+#     (np.random.normal(0, 1, 90), np.random.normal(-0.02, 1, 85)),# Experiment 3
+#     (np.random.normal(0, 1, 50), np.random.normal(0.03, 1, 60)), # Experiment 4
+# ]
+
+# -------------------------
+# Step 1: Pool all A and B points
+# -------------------------
+all_A = np.concatenate([exp for exp in DISTS_F3_KO[0]])
+all_B = np.concatenate([exp for exp in DISTS_A12_KO[0]])
+print(f"Total points pooled: A={len(all_A)}, B={len(all_B)}")
+
+# -------------------------
+# Step 2: Compute medians and bootstrap CI
+# -------------------------
+def bootstrap_median_ci(data, n_boot=1000, alpha=0.05):
+    medians = []
+    for _ in range(n_boot):
+        sample = np.random.choice(data, size=len(data), replace=True)
+        medians.append(np.median(sample))
+    lower = np.percentile(medians, 100*alpha/2)
+    upper = np.percentile(medians, 100*(1-alpha/2))
+    return np.median(data), lower, upper
+
+med_A, ci_low_A, ci_up_A = bootstrap_median_ci(all_A)
+med_B, ci_low_B, ci_up_B = bootstrap_median_ci(all_B)
+print(f"\nPooled Medians:")
+print(f"  A median = {med_A:.3f} [{ci_low_A:.3f}, {ci_up_A:.3f}]")
+print(f"  B median = {med_B:.3f} [{ci_low_B:.3f}, {ci_up_B:.3f}]")
+
+# -------------------------
+# Step 3: KS test on pooled data
+# -------------------------
+stat, pval_ks = ks_2samp(all_A, all_B)
+print(f"\nKS test (pooled) = statistic {stat:.3f}, p-value {pval_ks:.4e}")
+
+# -------------------------
+# Step 4: Permutation test for pooled median difference
+# -------------------------
+def permutation_test(A, B, n_permutations=10000):
+    obs_diff = np.median(A) - np.median(B)
+    pooled = np.concatenate([A, B])
+    count = 0
+    for _ in range(n_permutations):
+        np.random.shuffle(pooled)
+        new_A = pooled[:len(A)]
+        new_B = pooled[len(A):]
+        if abs(np.median(new_A) - np.median(new_B)) >= abs(obs_diff):
+            count += 1
+    return count / n_permutations
+
+perm_pval = permutation_test(all_A, all_B)
+print(f"Permutation test p-value (pooled) = {perm_pval:.4e}")
+
+# -------------------------
+# Step 5: ECDF plot
+# -------------------------
+def ecdf(data):
+    x = np.sort(data)
+    y = np.arange(1, len(x)+1)/len(x)
+    return x, y
+
+xA, yA = ecdf(all_A)
+xB, yB = ecdf(all_B)
+
+plt.figure(figsize=(6,4))
+plt.step(xA, yA, where='post', label="Population A (pooled)", color='C0')
+plt.step(xB, yB, where='post', label="Population B (pooled)", color='C1')
+plt.xlabel("Radial metric R")
+plt.ylabel("ECDF")
+plt.title("Pooled Populations ECDF")
+plt.legend()
+plt.tight_layout()
+plt.show()
